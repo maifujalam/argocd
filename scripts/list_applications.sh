b3a079ac31f50ec0ad89019f7f6af85e9530024f
alam <sk.maifujalam@gmail.com>
2023-10-07 16:29:12 +0530

    argocd tls termination

--- scripts/list_applications.sh
+++ scripts/list_applications.sh
@@ -2,9 +2,9 @@
 ARGOCD_SERVER=argocd.master.com
 USERNAME=admin
 PASSWORD=$(kubectl -n argo-cd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-
+echo $PASSWORD
 
 argocd login $ARGOCD_SERVER --grpc-web  --skip-test-tls --username $USERNAME --password $PASSWORD
 
-argocd app list
-argocd proj list
+#argocd app list
+#argocd proj list
