1819a89b2b09737cfe208f1831b5fca58b4ef00b
maifujalam <sk.maifujalam@gmail.com>
2024-08-31 00:10:13 +0530

    argocd server

--- scripts/list_applications.sh
+++ scripts/list_applications.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ARGOCD_SERVER=argocd.master.com
-ARGOCD_SERVER="localhost:8080"
+ARGOCD_SERVER="argocd.master.com"
 USERNAME=admin
 PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 # echo $PASSWORD
